36fcfea3c8b005b91c779010a46e1f9ed6c1e639
tedba742 <157366463+tedba742@users.noreply.github.com>
2024-05-28 13:51:12 +0200

    day 2 - 2/2

--- day2/day2part1.cpp
+++ day2/day2part1.cpp
@@ -4,18 +4,25 @@
 
 int main() {
     std::ifstream inputFile("input.txt");
+    if (!inputFile) {
+        std::cerr << "Failed to open input file." << std::endl;
+        return 1;
+    }
+
     std::string command{""};
     int value{0};
     int horizontalPosition{0};
     int depth{0};
+    int aim{0};
 
     while (inputFile >> command >> value) {
         if (command == "forward") {
             horizontalPosition += value;
+            depth += aim * value;
         } else if (command == "down") {
-            depth += value;
+            aim += value;
         } else if (command == "up") {
-            depth -= value;
+            aim -= value;
         }
     }
 
@@ -24,5 +31,4 @@ int main() {
     int result = horizontalPosition * depth;
     std::cout << "Result (horizontal position * depth): " << result << std::endl;
 
-    return 0;
 }
